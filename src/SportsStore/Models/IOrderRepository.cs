a679f3a1a24cc7ead50691c28f5192d69afa66ef
liefu zhang <liefuzhangnz@gmail.com>
2017-05-25 22:49:55 +1200

    284

---
+++ src/SportsStore/Models/IOrderRepository.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace SportsStore.Models {
+    public interface IOrderRepository {
+        IEnumerable<Order> Orders { get; }
+        void SaveOrder(Order order);
+    }
+}
